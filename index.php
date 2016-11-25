8a615751e70005f3c95ee6300e25b455e3dafa01
Ezelev <iiezelev@gmail.com>
2016-11-25 14:46:03 +0300

    Update index.php
    1) Убрана проверка is_array, т.к. не имеет смысла
    2) Убрано сложение $newArr = $newArr + $value, т.к в случае если $value строка, то в массив будет записан 0

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-$data1 = [
-    'parent.child.field.value' => 1,
+<?
+$data1 = ['parent.child.field.value' => 1,
     'parent.child.field2' => 2,
     'parent2.child.name' => 'test',
     'parent2.child2.name' => 'test',
     'parent2.child2.position' => 10,
     'parent3.child3.position' => 10,
-];
+    'keydefault' => 100];
 
 $sep = ".";
 
@@ -20,16 +20,9 @@ function separateArray(array $input,$sep)
         {
             $newArr = &$newArr[$sepKey];
         }
-        if (is_array($value))
-        {
-            separateArray($value);
-        }
-        else {
-            $newArr = $newArr + $value;
-        }
+            $newArr = $value;
     }
     return $result;
 }
 
-$res = separateArray($data1,$sep);
-var_dump($res);
+print_r(separateArray($data1,$sep));
